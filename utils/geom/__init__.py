394c3a06cd5f2ae0ea19844d757042c022ff3e91
mailmindlin <mailmindlin@users.noreply.github.com>
2023-02-15 22:58:06 -0500

    Better exports

--- utils/geom/__init__.py
+++ utils/geom/__init__.py
@@ -1,2 +1,4 @@
 from .util import Interpolable
 from .quaternion import Quaternion
+from .geom2 import Rotation2D, Translation2D, Transform2D, Twist2D, Pose2D
+from .geom3 import Rotation3D, Translation3D, Transform3D, Twist3D, Pose3D
