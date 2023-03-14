2379ec9da6e94ac6f7ca123e3915b1b5e7657d92
ajpiano12 <60827309+ajpiano12@users.noreply.github.com>
2023-03-13 23:50:03 -0400

    Moved tag back to compatible version

--- tag.py
+++ tag.py
@@ -1,36 +1,18 @@
 import json
-from utils.geom.geom3 import Pose3D, Translation3D, Rotation3D
-from utils.geom.quaternion import Quaternion
+import numpy as np
 
 tagf = open('tags.json')
 
 data = json.load(tagf)
 
-def parse_tag(tag):
-    pose = tag['pose']
-    posvec = [float(pos) for pos in pose['translation'].values()]
+tagpos = [0]*9
+rot0 = [0]*9
 
-    translation = Translation3D(
-        pose['translation']['x'],
-        pose['translation']['y'],
-        pose['translation']['z'],
-    )
-
-    quat = pose['rotation']['quaternion']
-    rotation = Rotation3D(
-        Quaternion(
-            quat['W'],
-            quat['X'],
-            quat['Y'],
-            quat['Z'],
-        )
-    )
-    return Pose3D(
-        translation,
-        rotation,
-    )
-
-tags = {
-    tag['ID']: parse_tag(tag)
-    for tag in data['tags']
-}
+for tag in data["tags"]:
+    posvec = []
+    for pos in tag['pose']['translation'].values():
+        posvec += [float(pos)]
+    posvec = np.array(posvec)
+    tagpos[tag['ID']] = posvec
+    if tag['pose']['rotation']['quaternion']['W']:
+        rot0[tag['ID']] = 1
