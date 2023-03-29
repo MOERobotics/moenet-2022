e243b08c0f2e84f219959fdbd4e238bea63e9834
ajpiano12 <60827309+ajpiano12@users.noreply.github.com>
2023-03-28 22:53:47 -0400

    Set up detector

--- apriltags.py
+++ apriltags.py
@@ -5,16 +5,26 @@ import moe_apriltags as apriltag
 
 class detection:
     #Tag detected
-    tag_num : int
+    tag_id : int
 
-    #Tag position from robot
-    relative_pose : Pose3D
+    #Tag position from robot in robot space
+    relative_pose : Transform3D
 
-    #Estimated field position
+    #Estimated field position of robot
     absolute_pose: Pose3D
 
     #Error of measurement
     error : float
+
+    @classmethod
+    def convert(cdetection):
+        converted = detection()
+        converted.tag_id = cdetection.tag_id
+        converted.relative_pose = Transform3D(
+            Translation3D(detection.pose_t[:,0]),
+            rotation=Rotation3D.from_rotation_matrix(detection.pose_R)
+        )
+        return converted
 
 class estimated_pose:
     absolute_pose : Pose3D
@@ -38,15 +48,72 @@ def network_format(position : estimated_pose) -> list[float]:
     return [*position.absolute_pose.translation.as_vec(),
             *position.absolute_pose.rotation.to_quaternion()]
 
+import depthai as dai
 class TagDetector:
     def __init__(self, cam) -> None:
         self.detector = apriltag.Detector(families="tag16h5", nthreads=2)
         self.cam = cam
+        self.monoq = cam.device.getOutputQueue(name="mono", maxSize=1, blocking=False)
+        calibdata =  cam.device.readCalibration()
+        intrinsics = calibdata.getCameraIntrinsics(dai.CameraBoardSocket.LEFT, destShape=(3*cam.resolution//2,cam.resolution))
+        self._camera_params = (
+            intrinsics[0][0],
+            intrinsics[1][1],
+            intrinsics[0][2],
+            intrinsics[1][2],
+        )
 
-    def detect(self, cam) -> list[tuple[int, Transform3D]]:
-        return []
+    def detect(self) -> list[detection]:
+        frame: 'dai.ImgFrame' = self.monoq.get()
+        img: np.ndarray = frame.getCvFrame()
+        detections: list[apriltag.Detection] = self.detector.detect(
+            img,
+            l=1,
+            r=8,
+            maxhamming=0,
+            estimate_tag_pose=True,
+            tag_size=.1524,
+            camera_params=self._camera_params,
+        )
+
+        final_detections = []
+        for tag in detections:
+            converted : detection = detection.convert(tag)
+            tag_cs = detection.relative_pose
+
+            #Camera in robot space
+            cam_robot = self.cam.pose
+
+            #tag camera space - z axis is in the normal direction from robot space
+            tagcs_robot = cam_robot.transform_by(tag_cs)
+
+            #What tag camera space looks like in tag space
+            tagcs_ts : Pose3D = Pose3D(Translation3D(0,0,0),
+                                Rotation3D.from_rotation_matrix(np.array(
+                                    [[0, 0,-1],
+                                    [1, 0, 0],
+                                    [0,-1, 0]]
+                                )))
+
+            #What tag space looks like in tag camera space
+            tag_tagcs = -tagcs_ts
+
+            tag_robot = tagcs_robot.transform_by(tag_tagcs)
+
+            #tag position in field space
+            tag_fs = tag.tags[converted.tag_id]
+
+            #robot position in field space
+            robot_fs = tag_fs.transform_by(-tag_robot)
+
+            converted.relative_pose = tag_robot
+            converted.absolute_pose = robot_fs
+
+            final_detections.append(converted)
+        return final_detections    
     
     def __enter__(self):
+        
         return self
 
     def __exit__(self, *args):
